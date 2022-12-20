5d7e8e67c0d8d25e1457d83e635397404dec5ff5
qeeqez <qeeqez@gmail.com>
2022-12-20 23:08:47 +0300

    Support in chat commands

--- src/main/java/com/qeeqez/ekpaantalyabot/handlers/impl/CommandHandler.java
+++ src/main/java/com/qeeqez/ekpaantalyabot/handlers/impl/CommandHandler.java
@@ -1,11 +1,13 @@
 package com.qeeqez.ekpaantalyabot.handlers.impl;
 
+import com.qeeqez.ekpaantalyabot.config.TelegramBotConfig;
 import com.qeeqez.ekpaantalyabot.messages.StartMessage;
 import com.qeeqez.ekpaantalyabot.handlers.IHandler;
 import com.qeeqez.ekpaantalyabot.bot.TelegramMessageSender;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
@@ -15,6 +17,9 @@ public class CommandHandler implements IHandler {
 
     @Autowired
     private TelegramMessageSender messageSender;
+
+    @Autowired
+    private TelegramBotConfig botConfig;
 
     final String ERROR_COMMAND_WAS_NOT_RECOGNIZED = "Sorry, command was not recognized";
 
@@ -33,6 +38,10 @@ public class CommandHandler implements IHandler {
         Message message = update.getMessage();
         Long chatId = message.getChatId();
         String command = message.getText();
+
+        if(command.contains("@" + botConfig.getUserName())) {
+            command = command.substring(0, command.indexOf("@"));
+        }
 
         switch (command) {
             case "/start", "/menu" -> messageSender.sendMessage(new StartMessage(chatId));

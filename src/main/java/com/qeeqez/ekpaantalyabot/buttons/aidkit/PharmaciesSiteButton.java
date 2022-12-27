3cce148e54febd723351fcab2986ccf037c44bbb
qeeqez <qeeqez@gmail.com>
2022-12-27 21:56:48 +0300

    Add pharmacies information

---
+++ src/main/java/com/qeeqez/ekpaantalyabot/buttons/aidkit/PharmaciesSiteButton.java
@@ -0,0 +1,19 @@
+package com.qeeqez.ekpaantalyabot.buttons.aidkit;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+
+@Getter
+@Setter
+public class PharmaciesSiteButton extends InlineKeyboardButton {
+
+    private final String openURL = "https://www.antalyaeo.org.tr/tr/nobetci-eczaneler";
+
+    private final String text = "🌎 Сайт с дежурными аптеками\n";
+
+    public PharmaciesSiteButton() {
+        setText(text);
+        setUrl(openURL);
+    }
+}

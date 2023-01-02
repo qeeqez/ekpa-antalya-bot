c19db7fc659d6c2aeec1462f1e56c8f13d8fce85
qeeqez <qeeqez@gmail.com>
2023-01-02 22:23:10 +0300

    Fix: Correct location

--- src/main/java/com/qeeqez/ekpaantalyabot/messages/directions/CityHallMessage.java
+++ src/main/java/com/qeeqez/ekpaantalyabot/messages/directions/CityHallMessage.java
@@ -16,7 +16,7 @@ public class CityHallMessage extends EditMessageText {
             ℹ️ В данном заведении можно получить справку *Нумаратаж* \\(Numarataj\\), необходимую для получения ВНЖ
             ⚡️ На первом этаже можно сделать копии документов за 0\\.5 TR
 
-            📍 [Kepez Belediye](https://goo.gl/maps/Fb68nczEehmjzf4K9) \\(3\\.5 км\\)
+            📍 [Kepez Belediye](https://goo.gl/maps/5WBP63uMR5dCa1HcA) \\(3\\.5 км\\)
             """;
 
     private CityHallMessage() {

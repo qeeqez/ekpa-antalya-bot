8ddebf347d752729e7c02cbda7c2a3fea124d733
qeeqez <qeeqez@gmail.com>
2023-01-09 21:54:43 +0300

    Fix: correct section name

--- src/main/java/com/qeeqez/ekpaantalyabot/messages/directions/PostOfficeMessage.java
+++ src/main/java/com/qeeqez/ekpaantalyabot/messages/directions/PostOfficeMessage.java
@@ -19,8 +19,8 @@ public class PostOfficeMessage extends EditMessageText {
             📍 [PTT Teomanpasa](https://goo.gl/maps/xq1qT8CjHqhzXtKx9) \\(2\\.1 км\\)
             📍 [PTT Sutculer](https://goo.gl/maps/nd8XwFx2EbUYSCj57) \\(2\\.9 км\\)
             
-            ℹ️ Где еще можно оплатить коммунальные услуги читайте в разделе
-            "Информация \\-\\> Как Оплатить?"
+            ℹ️ Где еще можно оплатить коммунальные услуги читайте в разделе:
+            ⭐️ Информация \\-\\> 🌟 Все о Ekpa \\-\\> 💰 Как Оплатить?
             """;
 
     private PostOfficeMessage() {

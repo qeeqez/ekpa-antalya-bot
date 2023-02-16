9d896be54c5bf9749dacabe65920e75c6a930765
qeeqez <qeeqez@gmail.com>
2023-02-16 20:13:46 +0300

    Update chats

--- src/main/java/com/qeeqez/ekpaantalyabot/buttons/chats/EkpaMainChatAttentionButton.java
+++ src/main/java/com/qeeqez/ekpaantalyabot/buttons/chats/EkpaMainChatAttentionButton.java
@@ -6,9 +6,9 @@ import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKe
 
 @Getter
 @Setter
-public class EkpaMainChatAttentionButton extends InlineKeyboardButton{
+public class EkpaMainChatAttentionButton extends InlineKeyboardButton {
     private final String openURL = "https://t.me/+14s_2-3hZzVmODAy";
-    private final String text = "🎄 ЧАТ БЕЗ РЕКЛАМЫ 🎄";
+    private final String text = "🏠 ЧАТ БЕЗ РЕКЛАМЫ 🏠";
 
     public EkpaMainChatAttentionButton() {
         setText(text);
--- src/main/java/com/qeeqez/ekpaantalyabot/markup/OurChatsMarkup.java
+++ src/main/java/com/qeeqez/ekpaantalyabot/markup/OurChatsMarkup.java
@@ -13,20 +13,19 @@ import java.util.List;
 
 @Getter
 @Setter
-public class OurChatsMarkup extends InlineKeyboardMarkup{
+public class OurChatsMarkup extends InlineKeyboardMarkup {
 
     public OurChatsMarkup() {
         List<List<InlineKeyboardButton>> rowsInLine = new ArrayList<>();
 
         rowsInLine.add(List.of(new BlocksChatButton(), new EkpaMainChatButton()));
-        rowsInLine.add(List.of(new TaxiChatButton(), new TradeChatButton()));
-        rowsInLine.add(List.of(new BeautyChatButton(), new ServicesChatButton()));
-        rowsInLine.add(List.of(new PartyChatButton()));
-        rowsInLine.add(List.of(new SportGamesChatButton(), new SportChatButton()));
-        rowsInLine.add(List.of(new EnglishClubChatButton()));
-        rowsInLine.add(List.of(new ITChatButton(), new GamingChatButton(), new BoardGamesChatButton()));
+        rowsInLine.add(List.of(new TradeChatButton()));
+        rowsInLine.add(List.of(new TaxiChatButton()));
         rowsInLine.add(List.of(new ParentsChatButton()));
         rowsInLine.add(List.of(new AnimalsChatButton()));
+        rowsInLine.add(List.of(new SportGamesChatButton(), new SportChatButton()));
+        rowsInLine.add(List.of(new PartyChatButton(), new BoardGamesChatButton()));
+        rowsInLine.add(List.of(new GamingChatButton(), new ITChatButton()));
         rowsInLine.add(List.of(new MainMenuButton()));
 
         setKeyboard(rowsInLine);

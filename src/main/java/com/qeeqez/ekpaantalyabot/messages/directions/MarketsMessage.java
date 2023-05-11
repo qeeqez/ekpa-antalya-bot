56ef2cb02c538f34967eca289894207ed65b6e1f
qeeqez <qeeqez@gmail.com>
2023-05-11 23:15:56 +0300

    Change markets order

--- src/main/java/com/qeeqez/ekpaantalyabot/messages/directions/MarketsMessage.java
+++ src/main/java/com/qeeqez/ekpaantalyabot/messages/directions/MarketsMessage.java
@@ -18,8 +18,8 @@ public class MarketsMessage extends EditMessageText {
             📍[Рынок Demirgül](https://goo.gl/maps/uqxp8L4RscSe2jpM7) \\(Среда, 1\\.7 км\\)
             📍[Рынок Göksu](https://goo.gl/maps/WakaAx3KK4Vmz9ELA) \\(Четверг, 1\\.7 км\\)
             📍[Рынок Sütçüler](https://goo.gl/maps/68DWmnXecWeFLbj4A) \\(Четверг, 2\\.2 км\\)
-            📍[Рынок Güneş](https://goo.gl/maps/TjswY6NAtn8e6G6v6) \\(Суббота, 1\\.4 км\\)
             📍[Рынок Teomanpaşa](https://goo.gl/maps/nRZKwLYFBEuVV6BJ7) \\(Пятница, 2\\.5 км\\)
+            📍[Рынок Güneş](https://goo.gl/maps/TjswY6NAtn8e6G6v6) \\(Суббота, 1\\.4 км\\)
             """;
 
     private MarketsMessage() {

9634ffc4d2d2629c41206d4ea22aff1ea76f501e
qeeqez <qeeqez@gmail.com>
2023-07-26 18:29:03 +0300

    Update close markets message

--- src/main/java/com/qeeqez/ekpaantalyabot/messages/directions/MarketsMessage.java
+++ src/main/java/com/qeeqez/ekpaantalyabot/messages/directions/MarketsMessage.java
@@ -13,13 +13,23 @@ public class MarketsMessage extends EditMessageText {
     private final String text = """
             *🧺 Ближайшие рынки:*
                         
-            📍[Рынок Ersoy](https://goo.gl/maps/6yhUhbb9Zpijy76JA) \\(Понедельник, 2\\.5 км\\)
-            📍[Рынок Beşkonaklılar](https://goo.gl/maps/RGH5rS8h7b2AMfCU6) \\(Среда до обеда, 500 м\\)
-            📍[Рынок Demirgül](https://goo.gl/maps/uqxp8L4RscSe2jpM7) \\(Среда, 1\\.7 км\\)
-            📍[Рынок Göksu](https://goo.gl/maps/WakaAx3KK4Vmz9ELA) \\(Четверг, 1\\.7 км\\)
-            📍[Рынок Sütçüler](https://goo.gl/maps/68DWmnXecWeFLbj4A) \\(Четверг, 2\\.2 км\\)
-            📍[Рынок Teomanpaşa](https://goo.gl/maps/nRZKwLYFBEuVV6BJ7) \\(Пятница, 2\\.5 км\\)
-            📍[Рынок Güneş](https://goo.gl/maps/TjswY6NAtn8e6G6v6) \\(Суббота, 1\\.4 км\\)
+            *Понедельник*
+            📍[Рынок Ersoy](https://goo.gl/maps/6yhUhbb9Zpijy76JA) \\(2\\.5 км\\)
+                        
+            *Среда*
+            📍[Рынок Beşkonaklılar](https://goo.gl/maps/RGH5rS8h7b2AMfCU6) \\(500 м, до обеда\\)
+            📍[Рынок Demirgül](https://goo.gl/maps/uqxp8L4RscSe2jpM7) \\(1\\.7 км\\)
+            📍[Рынок Sütçüler](https://goo.gl/maps/yFbwpSdE4yNt7ojH7) \\(3 км\\)
+                        
+            *Четверг*
+            📍[Рынок Göksu](https://goo.gl/maps/WakaAx3KK4Vmz9ELA) \\(1\\.7 км\\)
+            📍[Рынок Sütçüler](https://goo.gl/maps/68DWmnXecWeFLbj4A) \\(2\\.2 км\\)
+                        
+            *Пятница*
+            📍[Рынок Teomanpaşa](https://goo.gl/maps/nRZKwLYFBEuVV6BJ7) \\(2\\.5 км\\)
+                        
+            *Суббота*
+            📍[Рынок Güneş](https://goo.gl/maps/TjswY6NAtn8e6G6v6) \\(1\\.4 км\\)
             """;
 
     private MarketsMessage() {

7df858f5d2a8c2a474075442bc09b6cdf370837c
qeeqez <qeeqez@gmail.com>
2023-09-12 15:53:43 +0300

    Update barbershop prices

--- src/main/java/com/qeeqez/ekpaantalyabot/messages/directions/BarberMessage.java
+++ src/main/java/com/qeeqez/ekpaantalyabot/messages/directions/BarberMessage.java
@@ -14,18 +14,14 @@ public class BarberMessage extends EditMessageText {
             *Барбершопы*
                         
             📍 [NZM Salonu](https://goo.gl/maps/yePJMmDWHJDqcCSS7)
-            Стрижка \\+ Мойка головы 80 TL\\.
-            Стрижка \\+ Мойка \\+ Бритье 100 TL\\.
+            Стрижка \\+ Мойка головы 100 TL\\.
                         
             📍 [Yigit Erkek Kuaforu](https://goo.gl/maps/KUYj8pheja9Y2QST6)
-            Стрижка \\+ Мойка \\+ Массаж \\+ Скраб \\+ Бритье 140 TL\\.
                         
             📍 [Barber by Ahmet](https://goo.gl/maps/uvpYxdxAJTF1bfYx5)
-            Стрижка без бороды 80 TL\\.
-            Стрижка \\+ Мойка \\+ Маска \\+ Бритье \\+ Кола 150 TL\\.
-            
+            Стрижка \\+ Мойка головы 120 TL\\.
+                        
             📍 [Рядом с Migros](https://goo.gl/maps/PiBvkRPMak7bNo23A)
-            Стрижка \\+ Мойка \\+ Массаж \\+ Скраб \\+ Депиляция воском \\+ Бритье 150 TL\\.
             """;
 
     private BarberMessage() {

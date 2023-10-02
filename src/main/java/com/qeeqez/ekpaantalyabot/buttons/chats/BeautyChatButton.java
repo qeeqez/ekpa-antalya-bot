7c09d9ffdb80a87b9396ea27ca7d06394526e18a
qeeqez <qeeqez@gmail.com>
2023-10-02 11:10:41 +0300

    Update Beauty & Body chat link

--- src/main/java/com/qeeqez/ekpaantalyabot/buttons/chats/BeautyChatButton.java
+++ src/main/java/com/qeeqez/ekpaantalyabot/buttons/chats/BeautyChatButton.java
@@ -7,7 +7,7 @@ import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKe
 @Getter
 @Setter
 public class BeautyChatButton extends InlineKeyboardButton {
-    private final String openURL = "https://t.me/+YqOKkkp4ujIyYzEy";
+    private final String openURL = "https://t.me/+MbJwkYqxvlsxMGUy";
     private final String text = "💅 Beauty";
 
     public BeautyChatButton() {

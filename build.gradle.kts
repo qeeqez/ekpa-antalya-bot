9a1a43d049289738e1d0f3ceb198294a0af44c3c
qeeqez <qeeqez@gmail.com>
2023-11-30 12:23:27 +0300

    Move to JDK 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 group = "com.qeeqez"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+java.sourceCompatibility = JavaVersion.VERSION_21
 
 configurations {
     compileOnly {

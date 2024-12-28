18b30a33ee4b6916a50608550ccf6543303d334f
Maksim Sasnouski <qeeqez@gmail.com>
2024-12-28 15:59:27 +0400

    Bump libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.3.2"
-    id("io.spring.dependency-management") version "1.1.6"
+    id("org.springframework.boot") version "3.4.1"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "com.qeeqez"
@@ -19,6 +19,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
 
     implementation("org.springframework.boot:spring-boot-starter-webflux")
@@ -31,8 +32,8 @@ dependencies {
     testAnnotationProcessor("org.projectlombok:lombok")
 
 //    Telegram Bot
-    implementation("org.telegram:telegrambots-springboot-longpolling-starter:7.7.1")
-    implementation("org.telegram:telegrambots-client:7.7.1")
+    implementation("org.telegram:telegrambots-springboot-longpolling-starter:8.0.0")
+    implementation("org.telegram:telegrambots-client:8.0.0")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
